2fc673a99a0edfce6e7b1c1f5a0fcf68aabb947e
Guest User <Guest@Epicodus-8C.local>
2017-10-31 09:39:30 -0700

    add contact addresses

--- js/scripts.js
+++ js/scripts.js
@@ -14,20 +14,34 @@ Contact.prototype.fullName = function() {
   return this.firstName + ' ' + this.lastName;
 };
 
+Address.prototype.fullAddress = function() {
+  return this.street + ', ' + this.city + ', ' + this.state;
+};
+
 $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
+    debugger;
     event.preventDefault();
     var inputtedfirstName = $("input#new-first-name").val();
     var inputtedlastName = $("input#new-last-name").val();
+    var inputtedStreet = $("input#street").val();
+    var inputtedCity = $("input#city").val();
+    var inputtedState = $("input#state").val();
     var newContact = new Contact(inputtedfirstName, inputtedlastName);
+    var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
+    newContact.addresses.push(newAddress);
+    alert(newContact.addresses)
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $(".contact").last().click(function() {
-      alert("click!")
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
+      newContact.addresses.forEach(function(address){
+        $(".fullAddress").text(address.fullAddress());
+      });
+
     });
   });
 
